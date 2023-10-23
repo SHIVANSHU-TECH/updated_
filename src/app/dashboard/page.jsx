b2f1e245b9efb753168780bf00abf860d2743728
Satyam Srivastava <satyamsrivastava052@gmail.com>
2023-10-23 13:38:19 +0530

    updated

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,5 +1,5 @@
 import Sidebar from "@/components/DashboardSidebar/Sidebar";
-import Recent from "@/components/DashboardRecent/Recent";
+
 
 import React from "react";
 import css from "@/app/dashboard/dashboard.module.css";
@@ -10,7 +10,7 @@ const page = () => {
   return (
     <div className={css.dashboardContainer}>
       <Sidebar />
-<<<<<<< HEAD
+
       <div className={css.rightSideRecents}>
       <div className={css.recents}>
         <Recent heading="Events" data={8} />
@@ -20,9 +20,7 @@ const page = () => {
         <Transactions />
       </div>
       </div>
-=======
-      <Recent/>
->>>>>>> a1905e9f3a707821294f714ce4230c42b4e80b97
+
     </div>
   );
 };
